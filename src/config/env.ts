c1797b075313766ff71c8855e7b4684cc077ce24
unknown <swenpai.jayson@catcollege.edu.ph>
2024-11-19 15:13:45 +0800

    Remove .env from version control

--- src/config/env.ts
+++
@@ -1,5 +0,0 @@
-export const env = {
-  MONGODB_URI: import.meta.env.VITE_MONGODB_URI || 'mongodb+srv://rlawoffice:sXyXyJLnpfHFZ7uP@appointment-system.j7xag.mongodb.net/appointment',
-  JWT_SECRET: import.meta.env.VITE_JWT_SECRET || 'e8af1200637b9e3e56128ea3b5a768f34f8988186f4ce25a1a3f821cef7c71992186b39ea9925189dae62cb3d12c36229e030de6a7200e2fac9804375352cca9',
-  PORT: import.meta.env.VITE_PORT || 5000
-}; 
